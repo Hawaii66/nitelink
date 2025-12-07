ced15c46fe6404a2936ad7fae7f75d21d58873bb
Sebastian Ahlman <hawaiilive@outlook.com>
2025-12-07 23:15:40 +0100

    show url

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,5 +3,7 @@ import { createFileRoute } from '@tanstack/react-router'
 export const Route = createFileRoute('/')({ component: App })
 
 function App() {
-  return <h1>Hello worlds</h1>
+  return <h1>Hello worlds {JSON.stringify({
+	loc:window.location
+  },null,2)}</h1>
 }
