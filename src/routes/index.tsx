2b74707a3fbb8da2f63cd193f16adaac6e3a1236
Sebastian Ahlman <hawaiilive@outlook.com>
2025-12-07 23:35:43 +0100

    test host

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,9 +1,16 @@
+import { GetClub, GetHost } from '@/lib/host'
 import { createFileRoute } from '@tanstack/react-router'
 
 export const Route = createFileRoute('/')({ component: App })
 
 function App() {
-  return <h1>Hello worlds {JSON.stringify({
-	loc:window.location
-  },null,2)}</h1>
+	const host = GetHost()
+	const club = GetClub()
+	const baseURL = import.meta.env.BASE_URL
+  return <div>
+
+	host: {host}<br/>
+	club: {club}<br/>
+	baseurl: {baseURL}
+  </div>
 }
